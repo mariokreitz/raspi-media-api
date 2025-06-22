9ceb49b14c912fcd74f1fa0ae235ea53943af699
mariokreitz <Mario.Kreitz@web.de>
2025-06-22 22:42:45 +0200

    Feat: Adds series table to database
    Adds a new table to the database for storing series data, including indexes. This allows for more efficient querying and organization of series information. Also adds a seriesId column to the media table.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -37,7 +37,8 @@ export async function openDb() {
                        watched           INTEGER DEFAULT 0,
                        favorite          INTEGER DEFAULT 0,
                        playback_position INTEGER DEFAULT 0,
-                       last_played       TIMESTAMP
+                       last_played       TIMESTAMP,
+                       seriesId          INTEGER
                    )
     `);
 
@@ -48,6 +49,32 @@ export async function openDb() {
         CREATE INDEX IF NOT EXISTS idx_media_mediaType ON media (mediaType);
         CREATE INDEX IF NOT EXISTS idx_media_favorite ON media (favorite);
         CREATE INDEX IF NOT EXISTS idx_media_watched ON media (watched);
+        CREATE INDEX IF NOT EXISTS idx_media_seriesId ON media (seriesId);
+    `);
+
+    await db.exec(`CREATE TABLE IF NOT EXISTS series
+                   (
+                       id                INTEGER PRIMARY KEY AUTOINCREMENT,
+                       title             TEXT NOT NULL,
+                       original_name     TEXT,
+                       overview          TEXT,
+                       first_air_date    TEXT,
+                       poster_path       TEXT,
+                       backdrop_path     TEXT,
+                       genre             TEXT,
+                       original_language TEXT,
+                       origin_country    TEXT,
+                       popularity        REAL,
+                       vote_average      REAL,
+                       vote_count        INTEGER,
+                       mediaType         TEXT
+                   )
+    `);
+
+    await db.exec(`
+        CREATE INDEX IF NOT EXISTS idx_series_title ON series (title);
+        CREATE INDEX IF NOT EXISTS idx_series_genre ON series (genre);
+        CREATE INDEX IF NOT EXISTS idx_series_mediaType ON series (mediaType);
     `);
 
     return db;
